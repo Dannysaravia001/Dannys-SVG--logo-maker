650ee5bbcaeae451ef81fab0798f9e32fb7c80f2
unknown <dannysaravia100@gmail.com>
2023-12-20 12:22:51 -0500

    added first prompt to project

---
+++ Develop/Index.js
@@ -0,0 +1,23 @@
+const inquirer = require('inquirer');
+
+async function generateLogo() {
+  const userInput = await inquirer.prompt([
+    {
+      name: 'text',
+      message: 'Enter up to three characters:',
+      validate: (input) => input.length <= 3,
+    },
+    {
+      name: 'textColor',
+      message: 'Enter Text Color:',
+    },
+    {
+      name: 'shape',
+      type: 'list',
+      message: 'What shape do you want the logo to be?',
+      choices: ['circle', 'triangle', 'square'], 
+    },
+  ]);
+
+}
+generateLogo ();
